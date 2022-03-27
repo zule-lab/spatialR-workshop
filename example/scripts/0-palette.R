ba538805d1584b01f75209a32af7345caf206dc2
Isabella Richmond <icrichmond@mun.ca>
2022-03-27 18:12:29 -0400

    palette for map

---
+++ example/scripts/0-palette.R
@@ -0,0 +1,16 @@
+# credit to Alec Robitaille and WEEL's study-area-figs repo for inspiration/code: https://github.com/wildlifeevoeco/study-area-figs
+
+# Water
+watercol <- '#99acc3'
+
+# Land
+montrealcol <- '#ceb99b'
+canadacol <- '#ddc48d'
+
+# Ruelles Vertes
+rvcol <- '#1c3516'
+
+
+# Map etc
+gridcol1 <- '#323232'
+gridcol2 <- '#73776F'
